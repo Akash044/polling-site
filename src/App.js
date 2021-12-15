233b701590b054d40528cc52a61f202f78cd915e
Akash374 <akash.hossain@dataphilics.in>
2021-12-16 00:20:48 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import HomePage from './components/HomePage';
+import PrivateRoute from './components/PrivateRoute'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { createContext, useState } from 'react';
+import LoginPage from './components/LoginPage';
+import CreatePollPage from './components/CreatePollPage';
+import SignUpPage from './components/SignUpPage';
+import DisplayPolls from './components/DisplayPolls';
+export const UserContext = createContext();
 
 function App() {
+  const [user, setUser] = useState({});
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider value={[user, setUser]}>
+      <Router>
+        {/* <Header></Header> */}
+        <Switch>
+          <Route exact path="/">
+            <HomePage></HomePage>
+          </Route>
+          <Route path="/home">
+            <HomePage></HomePage>
+          </Route>
+          <Route path="/login">
+            <LoginPage></LoginPage>
+          </Route>
+          <Route path="/signup">
+            <SignUpPage></SignUpPage>
+          </Route>
+          <Route path="/polls">
+            <DisplayPolls />
+          </Route>
+
+          <PrivateRoute path="/createPoll">
+            <CreatePollPage />
+          </PrivateRoute>
+
+          <PrivateRoute path="/poll/:id">
+            <DisplayPolls />
+          </PrivateRoute>
+          
+        </Switch>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
